e597bcc9f75187b283c00a5d1ac48e4ae42f9010
pulu <pulu@pinduoduo.com>
2019-07-08 22:18:43 +0800

    Initial Commit

---
+++ Wine_Searcher/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Wine_Searcher
+//
+//  Created by Zhou Wei Ran on 2019/7/8.
+//  Copyright © 2019 Paper Scratch. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
